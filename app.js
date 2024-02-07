2c09113e46ed05795a3a3bb5379c1c8f8541927b
John Shutt <pemulis@users.noreply.github.com>
2024-02-06 23:13:33 -0800

    add 202 status code if login match found
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- app.js
+++ app.js
@@ -279,9 +279,9 @@ app.post('/login', [
     const result = await pool.query("SELECT * FROM companions WHERE name = $1", [name]);
     if (result.rows.length > 0) {
       const companion = result.rows[0];
-      const match = await bcrypt.compare(password, companion.hashedpassword);
+      const match = await bcrypt.compare(password, companion.hashedPassword);
       if (match) {
-        res.send("Logged in successfully");
+        res.status(202).send("Logged in successfully");
       } else {
         res.status(401).send("Password is incorrect");
       }
