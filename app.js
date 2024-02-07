216ca5c77489deb76bd6f41d63303bca118b5045
John Shutt <pemulis@users.noreply.github.com>
2024-02-06 16:13:32 -0800

    log
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ async function callAssistant(messages, prompt, assistant, thread) {
   let completedThread = await openai.beta.threads.messages.list(thread.id);
   let newMessages = completedThread.data.slice();
   for (let message of newMessages) {
-    console.log("message:", message.content.text.value);
+    console.log("message:", message.content);
     saveMessage(assistant.name, message.text.value, assistant.id, thread.id);
   }
   messages = messages.slice(originalMessageLength);
