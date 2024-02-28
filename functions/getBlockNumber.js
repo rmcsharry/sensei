cfb47b4d73da4a4a30e4f86bc94b6c98f2c5ee81
John Shutt <pemulis@users.noreply.github.com>
2024-02-27 18:21:45 -0800

    let's not use an external API call for test function
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- functions/getBlockNumber.js
+++ functions/getBlockNumber.js
@@ -1,18 +1,5 @@
-// Imports the Alchemy SDK
-const { Alchemy, Network } = require('alchemy-sdk');
-
-// Configures the Alchemy SDK
-const alchemyConfig = {
-  apiKey: process.env.ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
-
-// Creates an Alchemy object instance with the config to use for making requests
-const alchemy = new Alchemy(alchemyConfig);
-
-// Function to get the block number from Alchemy
 function getBlockNumber() {
-  return alchemy.core.getBlockNumber();
+  return 42069; // arbitrary block number for testing
 }
 
 module.exports = getBlockNumber;
