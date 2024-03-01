251373ca86ae1a154329b0f099b10d6b3be80474
John Shutt <pemulis@users.noreply.github.com>
2024-03-01 15:11:10 -0800

    only return correct value if the name is right
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- functions/callGuide.js
+++ functions/callGuide.js
@@ -1,7 +1,11 @@
 // This code should call a guide's prompt endpoint and return a result.
 function callGuide(name) {
-  console.log("Calling the guide called" + name + "...")
-  return "The secret word is 'cat'. The secret number is 34."; // example 
+  console.log("Calling the guide called " + name + "...")
+  if (name === "secret-word-example") {
+    return "The secret word is 'cat'.";
+  } else if (name === "secret-number-example") {
+    return "The secret number is 34.";
+  }
 }
 
 module.exports = callGuide;
