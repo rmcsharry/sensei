52038ac03c49c4305a334ef3e4c70955253acfa5
John Shutt <pemulis@users.noreply.github.com>
2024-03-01 14:42:47 -0800

    add secret number guide example
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- functions/callGuide.js
+++ functions/callGuide.js
@@ -1,7 +1,7 @@
 // This code should call a guide's prompt endpoint and return a result.
 function callGuide(uri) {
   console.log("Calling guide at " + uri + "...")
-  return "The secret word is 'cat'."; // example 
+  return "The secret word is 'cat'. The secret number is 34."; // example 
 }
 
 module.exports = callGuide;
