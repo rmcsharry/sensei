ba6d8a078f8865cfce8c70ae794fb0b2e9df7d6d
John Shutt <pemulis@users.noreply.github.com>
2024-03-03 10:00:27 -0800

    add prompt to callGuide
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- functions/callGuide.js
+++ functions/callGuide.js
@@ -1,11 +1,12 @@
 const sensei = require('../sensei.json');
 
 // This code should call a guide's prompt endpoint and return a result.
-async function callGuide(name) {
+async function callGuide(name, prompt) {
   const guideNames = sensei.guides.map(guide => guide.name);
   console.log("Available guides: " + guideNames.join(", "));
 
   console.log("Calling the guide called " + name + "...")
+  console.log("With the prompt: " + prompt + "...");
   if (name === "secret-word-example") {
     return "The secret word is 'cat'.";
   } else if (name === "secret-number-example") {
