6794100a868901ba80aed00920e81861b9572fe5
John Shutt <pemulis@users.noreply.github.com>
2024-06-20 15:16:41 -0700

    basic sendIntention function
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- functions/sendIntention.js
+++ functions/sendIntention.js
@@ -2,7 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config(); // Ensure that environment variables are loaded
 
 async function sendIntention(action) {
-
+  const intention = {
+    action: action,
+    bundler: process.env.BUNDLER_ADDRESS,
+    expiry: process.env.EXPIRY,
+    nonce: process.env.NONCE // need to track this internally, in the database
+  }
+  return intention;
 }
 
 export default sendIntention;
