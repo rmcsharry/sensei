bb5b2f1c6486f5b706bcddc5cf064f8d47ffafaf
John Shutt <pemulis@users.noreply.github.com>
2024-07-05 12:46:26 -0700

    display account holder prompt after signing or rejecting intention
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -238,10 +238,12 @@ const Home = () => {
       console.log('Intention processed:\n', result);
       setErrorMessage('');
       // Try creating a system prompt and calling sendPromptToBackend
+      displayPrompt("Thank you, I signed and sent the intention to the Oya bundler.");
       sendPromptToBackend("Thank you, I signed and sent the intention to the Oya bundler.");
     } catch (error) {
       console.error('Sign message error:', error);
       setErrorMessage(error.message);
+      displayPrompt("I'm sorry, I was unable to sign and send the intention to the Oya bundler. Either the intention did not match what I wanted to do, or there was a technical issue.");
       sendPromptToBackend("I'm sorry, I was unable to sign and send the intention to the Oya bundler. Either the intention did not match what I wanted to do, or there was a technical issue.")
     }
   };  
